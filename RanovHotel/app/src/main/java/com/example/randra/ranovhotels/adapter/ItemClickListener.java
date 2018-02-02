7e1e624f379134d4e2fc58abbfa337b0b13494f5
randra <randhisanjaya3@gmail.com>
2018-02-02 12:53:48 +0700

    Ranov Hotel

---
+++ RanovHotel/app/src/main/java/com/example/randra/ranovhotels/adapter/ItemClickListener.java
@@ -0,0 +1,11 @@
+package com.example.randra.ranovhotels.adapter;
+
+import android.view.View;
+
+/**
+ * Created by randra on 16/01/18.
+ */
+
+public interface ItemClickListener {
+    void onItemClick(View v, int pos);
+}
---
+++ RanovHotel/app/src/main/java/com/example/randra/ranovhotels/adapter/MyAdapter.java
@@ -0,0 +1,83 @@
+package com.example.randra.ranovhotels.adapter;
+
+import android.content.Context;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+
+import com.example.randra.ranovhotels.activity.R;
+import com.example.randra.ranovhotels.model.DaftarRuangan;
+
+import java.util.ArrayList;
+
+/**
+ * Created by randra on 16/01/18.
+ */
+
+public class MyAdapter extends RecyclerView.Adapter<MyHolder> implements Filterable {
+    Context c;
+    ArrayList<DaftarRuangan> ruangans,filterList;
+    CustomFilter filter;
+
+    public MyAdapter(ArrayList<DaftarRuangan> ruangans)
+    {
+
+        this.ruangans=ruangans;
+        this.filterList=ruangans;
+    }
+
+
+    @Override
+    public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //CONVERT XML TO VIEW ONBJ
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.model,null);
+
+        //HOLDER
+        MyHolder holder=new MyHolder(v);
+
+        return holder;
+    }
+
+    //DATA BOUND TO VIEWS
+    @Override
+    public void onBindViewHolder(MyHolder holder, int position) {
+
+        //BIND DATA
+        holder.posTxt.setText(Integer.toString(ruangans.get(position).getHarga()));
+        holder.nameTxt.setText(ruangans.get(position).getnama());
+
+
+
+
+
+        //IMPLEMENT CLICK LISTENET
+        holder.setItemClickListener(new ItemClickListener() {
+            @Override
+            public void onItemClick(View v, int pos) {
+                Snackbar.make(v,ruangans.get(pos).getnama(),Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    //GET TOTAL NUM OF PLAYERS
+    @Override
+    public int getItemCount() {
+        return ruangans.size();
+    }
+
+    //RETURN FILTER OBJ
+    @Override
+    public Filter getFilter() {
+        if(filter==null)
+        {
+            filter=new CustomFilter(filterList,this);
+        }
+
+        return filter;
+    }
+}
